00e7401c5c7c7efeafb47a7c1e2483e4443ecd4c
Dmitriy Nefedchenko <dnefedchenko@gmail.com>
2018-11-04 21:31:01 +0200

    Actuator endpoints added

--- frontend/src/main/ui/src/app/app.component.ts
+++ frontend/src/main/ui/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {MediaService, VolumeItem} from "./services/media.service";
+import {AlbumTrack, Item, MediaService, VolumeItem} from "./services/media.service";
 import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
@@ -8,12 +8,13 @@ import {FormBuilder, FormGroup} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  books: VolumeItem[];
+  items: Item[];
+
   mediaForm: FormGroup;
   pageLoadingCompleted: boolean =  false;
 
   constructor(private mediaService: MediaService, private formBuilder: FormBuilder) {
-    this.books = [];
+    this.items = [];
   }
 
   ngOnInit(): void {
@@ -28,14 +29,46 @@ export class AppComponent implements OnInit {
 
   search(): void {
     const searchTerm = this.mediaForm.get('searchTerm').value;
-    this.mediaService
-      .getMedia(searchTerm)
-      .then(books => {
-        this.books = books;
+
+    Promise
+      .all([this.getBooks(searchTerm), this.getAlbums(searchTerm)])
+      .then(values => {
+        const books = values[0];
+        const albums = values[1];
+        this.items = this.booksToItems(books);
+        this.items = this.items.concat(this.albumsToItems(albums));
+        this.items.sort((a, b) => {return a.title.localeCompare(b.title)});
         this.pageLoadingCompleted = true;
-      }, error => {
-        console.log(error);
-        this.pageLoadingCompleted = false;
-      });
+      },
+        error => {
+          console.log(error);
+          this.pageLoadingCompleted = false;
+        });
+  }
+
+  private getBooks(searchTerm: string): Promise<VolumeItem[]> {
+    return this.mediaService.getBooks(searchTerm);
+  }
+
+  private getAlbums(searchTerm: string): Promise<AlbumTrack[]> {
+    return this.mediaService.getAlbums(searchTerm);
+  }
+
+  private booksToItems(books: VolumeItem[]): Item[] {
+    return books
+      .map(book => this.toItem(book.volumeInfo.title, book.volumeInfo.authors, book.type));
+  }
+
+  private albumsToItems(albums: AlbumTrack[]): Item[] {
+    return albums
+      .map(album => this.toItem(album.trackName, album.artistName, album.type));
+  }
+
+  private toItem(title: string, author: any, type: string): Item {
+    return {
+      'title': title,
+      'author': author,
+      'type': type
+    };
   }
 }
--- frontend/src/main/ui/src/app/services/media.service.ts
+++ frontend/src/main/ui/src/app/services/media.service.ts
@@ -5,13 +5,26 @@ import {Injectable} from "@angular/core";
 export class MediaService {
   constructor(private httpClient: HttpClient) {}
 
-  public getMedia(term: string): Promise<Array<VolumeItem>> {
+  public getBooks(term: string): Promise<Array<VolumeItem>> {
     let params = new HttpParams();
     if (term) {
       params = params.append('term', term);
     }
     return this.httpClient
       .get<Array<VolumeItem>>('/api/books', {params: params})
+      .toPromise();
+  }
+
+  public getAlbums(term: string, countryCode?: string): Promise<AlbumTrack[]> {
+    let params = new HttpParams();
+    if (term) {
+      params = params.append('term', term);
+    }
+    if (countryCode) {
+      params = params.append('country', countryCode);
+    }
+    return this.httpClient
+      .get<AlbumTrack[]>('/api/albums', {params: params})
       .toPromise();
   }
 }
@@ -25,4 +38,16 @@ export interface VolumeItem {
 export interface VolumeInfo {
   title: string;
   authors: Array<string>;
+}
+
+export interface AlbumTrack {
+  trackName: string;
+  artistName: string;
+  type: string;
+}
+
+export interface Item {
+  title: string;
+  author: string;
+  type: string;
 }
